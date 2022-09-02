614a96e8a29dac9dc3fa6e300183194a7ee843a4
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-02 18:53:23 -0300

    se agrego los controles a la ruta

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const cityRouter = require('./cities');
 const userRouter = require('./user')
-
+const itineraryRouter = require('./itineraries')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyTinerary' });
@@ -10,5 +10,6 @@ router.get('/', function(req, res, next) {
 
 router.use('/cities',cityRouter)
 router.use('/auth',userRouter)
+router.use('/itineraries',itineraryRouter )
 
 module.exports = router;
