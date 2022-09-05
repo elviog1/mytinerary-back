2fe587b6d77ada0c336c3910befb995edb4f6bd8
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-05 19:41:10 -0300

    se agrego la ruta de comments

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const cityRouter = require('./cities');
 const userRouter = require('./user')
-const itineraryRouter = require('./itineraries')
+const itineraryRouter = require('./itineraries');
+const commentRouter = require('./comments');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyTinerary' });
@@ -11,5 +12,5 @@ router.get('/', function(req, res, next) {
 router.use('/cities',cityRouter)
 router.use('/auth',userRouter)
 router.use('/itineraries',itineraryRouter )
-
+router.use('/comments',commentRouter)
 module.exports = router;
