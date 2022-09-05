f916a256d85ef07d7de389878642045bfe813075
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-05 20:51:15 -0300

    se agrego las rutas de activity al index

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const cityRouter = require('./cities');
 const userRouter = require('./user')
 const itineraryRouter = require('./itineraries');
 const commentRouter = require('./comments');
+const activityRouter = require('./activities')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyTinerary' });
@@ -13,4 +14,6 @@ router.use('/cities',cityRouter)
 router.use('/auth',userRouter)
 router.use('/itineraries',itineraryRouter )
 router.use('/comments',commentRouter)
+router.use('/activities',activityRouter) 
 module.exports = router;
+ 
