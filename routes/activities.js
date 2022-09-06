be40f8756c7dee679fccdcf3e4112fe14890ee31
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-06 00:49:36 -0300

    se agregaron rutas de comments

--- routes/activities.js
+++ routes/activities.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
-const {create,all} = require('../controller/activityController')
+const {create,all,getActivityFromItinerary} = require('../controller/activityController')
 
 
 router.post('/',create)
 router.get('/', all)
+router.get('/query',getActivityFromItinerary)
 
 module.exports = router
