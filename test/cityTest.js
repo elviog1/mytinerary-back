1b4e6c5492c3350999af772eca766c63ee955da0
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-08 00:52:04 -0300

    test de get city

--- test/cityTest.js
+++ test/cityTest.js
@@ -2,32 +2,52 @@ const {assert} = require('chai')
 const request= require ('supertest')
 const app = require ('../app')
 
-describe('POST /city', function() {
-    it('Must respond with the id', function(done) {
-      request(app)
-      .post('/cities')
-      .send({name: "santadilla", image: 'hola.png', country: 'argentina papa', population:10000, fundation:'123'})
-      .then(response => {
-        assert.isString(response.body.id)
-        done()
-    })
-    }) 
+// describe('POST /city', function() {
+//     it('Must respond with the id', function(done) {
+//       request(app)
+//       .post('/cities')
+//       .send({name: "santadilla", image: 'hola.png', country: 'argentina papa', population:10000, fundation:'123'})
+//       .then(response => {
+//         assert.isString(response.body.id)
+//         done()
+//     })
+//     }) 
 
-    it('Must respond with the 201 status code', function(done) {
-      request(app)
-        .post('/cities')
-        .send({name: "santadilla", image: 'hola.png', country: 'argentina papa', population:10000, fundation:'123'})
-        .expect(201, done)
-    })
+//     it('Must respond with the 201 status code', function(done) {
+//       request(app)
+//         .post('/cities')
+//         .send({name: "santadilla", image: 'hola.png', country: 'argentina papa', population:10000, fundation:'123'})
+//         .expect(201, done)
+//     })
 
-    it('Must respond with 400 status code', function(done) {
+//     it('Must respond with 400 status code', function(done) {
+//       request(app)
+//         .post('/cities')
+//         .send({})
+//         .expect(400)
+//         .end(function(err, res){
+//           if (err) return done(err)
+//           return done()
+//         })
+//     });
+//   });
+
+  describe('GET /city', function(){
+    // it('Must respond with the cities', function(done){
+    //   request(app)
+    //   .get('/cities')
+    //   .auth('name')
+    //   .set('Accept','application/json')
+    //   .expect(201,done)
+    // })
+    it('Must respond with the id',function (done){
       request(app)
-        .post('/cities')
-        .send({})
-        .expect(400)
-        .end(function(err, res){
-          if (err) return done(err)
-          return done()
-        })
-    });
-  });
+      .get('/cities')
+      .auth('id')
+      .expect(201,done)
+    })
+  })
+
+
+
+
