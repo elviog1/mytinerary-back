08034f51429b2d96369106cf5b0c0b0b97ffd280
juanrivarola <jeryflr@gmail.com>
2022-09-09 18:42:36 -0300

    fix

--- models/User.js
+++ models/User.js
@@ -7,11 +7,11 @@ const schema = new mongoose.Schema({
     password:[{type: String, required: true}],
     photo:{type: String, required: true},
     country:{type: String, required: true},
-    role: {type: String, required:true},
-    from: [{type: String, required:true}],
-    logged: {type: Boolean, required:true},//por defecto false. cuando el usuario se logea => true
-    verified: {type: Boolean, required:true},//por defecto false. cuando el usuario verifica su cuenta =>true
-    code:{type: String, required:true},
+    role: {type: String, required: true},
+    from: [{type: String, required: true}],
+    logged: {type: Boolean, required: true},//por defecto false. cuando el usuario se logea => true
+    verified: {type: Boolean, required: true},//por defecto false. cuando el usuario verifica su cuenta =>true
+    code:{type: String, required: true},
 })
 const User = mongoose.model(
     'users', 
