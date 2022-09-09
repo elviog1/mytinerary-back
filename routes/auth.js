b5b10850e691192858ed5151a7ca616978910efb
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-09 19:36:44 -0300

    se agrego a la ruta el verifymail

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {create, all, signUp} = require('../controller/userController')
+const {create, all, signUp,verifyMail} = require('../controller/userController')
 
 /* GET users listing. */
 router.get('/', all)
 router.post('/signup', signUp)
+router.get('/:id',verifyMail)
 
 module.exports = router;
