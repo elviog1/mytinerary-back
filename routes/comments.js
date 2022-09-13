672f1f524a8b9f24070e4ebdad48ab53d5cc884a
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-13 19:46:35 -0300

    ruta para el update de comment

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 var express= require('express');
 var router = express.Router();
-const {create,all,getCommentFromItinerary} = require('../controller/commentController')
+const {create,all,getCommentFromItinerary,update} = require('../controller/commentController')
 
 router.post('/', create)
 router.get('/',all)
 router.get('/query',getCommentFromItinerary)
+router.put('/:id',update)
 
 module.exports = router
