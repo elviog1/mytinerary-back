b77785b0301ec5cc16fb89e346ab31072e43ca34
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-13 23:48:47 -0300

    se configuro la ruta de los metodos extras

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {create, all, signUp, verifyMail, signIn, signOut} = require('../controller/userController')
+const {create, all, signUp, verifyMail, signIn, signOut,update} = require('../controller/userController')
 
 /* GET users listing. */
 router.get('/', all)
@@ -8,5 +8,6 @@ router.post('/signup', signUp)
 router.post('/signin',signIn)
 router.get('/verify/:code', verifyMail)
 router.post('/signout', signOut)
+router.put('/:id',update)
 
 module.exports = router;
