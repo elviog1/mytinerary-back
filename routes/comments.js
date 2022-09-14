f97391a0bc19c9c6d2150fb15b58fa334052ff61
Elvio Galeano <elvio.galeano827@gmail.com>
2022-09-13 23:49:07 -0300

    se configuro la ruta de los metodos extras

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,11 @@
 var express= require('express');
 var router = express.Router();
-const {create,all,getCommentFromItinerary,update} = require('../controller/commentController')
+const {create,all,getCommentFromItinerary,update, oneComment,destroy} = require('../controller/commentController')
 
 router.post('/', create)
 router.get('/',all)
 router.get('/query',getCommentFromItinerary)
 router.put('/:id',update)
-
+router.get('/:id',oneComment)
+router.delete('/:id',destroy)
 module.exports = router
